df62f23e9f109bd3189f806b5e1187193fc32694
Manthan Sharma <72844165+man-than-shar-ma@users.noreply.github.com>
2021-10-24 22:45:35 +0530

    First Commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log('The bot is ready')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping'){
+        message.reply({
+            content:'Pong'
+        })
+    }
+})
+client.login(process.env.TOKEN)
+
+

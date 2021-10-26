4b21ddb78cecf11c73032bd334b4c2933bc22665
Manthan Sharma <72844165+man-than-shar-ma@users.noreply.github.com>
2021-10-26 21:35:51 +0530

    slash commands

--- index.js
+++ index.js
@@ -18,6 +18,42 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -32,13 +68,69 @@ var client = new discord_js_1.default.Client({
     ]
 });
 client.on('ready', function () {
+    var _a;
     console.log('The bot is ready');
-});
-client.on('messageCreate', function (message) {
-    if (message.content === 'ping') {
-        message.reply({
-            content: 'Pong'
-        });
+    var guildID = '901866629485436968';
+    var guild = client.guilds.cache.get(guildID);
+    var commands;
+    if (guild) {
+        commands = guild.commands;
     }
+    else {
+        commands = (_a = client.application) === null || _a === void 0 ? void 0 : _a.commands;
+    }
+    commands === null || commands === void 0 ? void 0 : commands.create({
+        name: 'ping',
+        description: 'Replies with pong'
+    });
+    commands === null || commands === void 0 ? void 0 : commands.create({
+        name: 'add',
+        description: 'Adds two numbers',
+        options: [{
+                name: 'num1',
+                description: 'The first number.',
+                required: true,
+                type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'The second number.',
+                required: true,
+                type: discord_js_1.default.Constants.ApplicationCommandOptionTypes.NUMBER
+            }]
+    });
 });
+client.on('interactionCreate', function (interaction) { return __awaiter(void 0, void 0, void 0, function () {
+    var commandName, options, num1, num2;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!interaction.isCommand()) {
+                    return [2 /*return*/];
+                }
+                commandName = interaction.commandName, options = interaction.options;
+                if (!(commandName === 'ping')) return [3 /*break*/, 1];
+                interaction.reply({
+                    content: 'pong',
+                    //ephemeral: true
+                });
+                return [3 /*break*/, 4];
+            case 1:
+                if (!(commandName === 'add')) return [3 /*break*/, 4];
+                num1 = options.getNumber('num1');
+                num2 = options.getNumber('num2');
+                return [4 /*yield*/, interaction.deferReply({})];
+            case 2:
+                _a.sent();
+                return [4 /*yield*/, new Promise(function (resolve) { return setTimeout(resolve, 5000); })];
+            case 3:
+                _a.sent();
+                interaction.editReply({
+                    content: "the sum is " + (num1 + num2)
+                });
+                _a.label = 4;
+            case 4: return [2 /*return*/];
+        }
+    });
+}); });
 client.login(process.env.TOKEN);
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import DiscordJS, { Intents } from 'discord.js'
+import DiscordJS, { Intents, Interaction } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -11,15 +11,69 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log('The bot is ready')
+
+    const guildID = '901866629485436968'
+    const guild = client.guilds.cache.get(guildID)
+    let commands
+
+    if(guild){
+        commands = guild.commands
+    }
+    else{
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'ping',
+        description: 'Replies with pong'
+    })
+
+    commands?.create({
+        name: 'add',
+        description: 'Adds two numbers',
+        options: [{
+            name: 'num1',
+            description: 'The first number.',
+            required: true,
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+        },
+        {
+            name: 'num2',
+            description: 'The second number.',
+            required: true,
+            type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+        }]
+    })
 })
 
-client.on('messageCreate', (message) => {
-    if (message.content === 'ping'){
-        message.reply({
-            content:'Pong'
+client.on('interactionCreate', async (interaction) => {
+    if(!interaction.isCommand()){
+        return
+    }
+    const {commandName,options} = interaction
+
+    if(commandName === 'ping'){
+        interaction.reply({
+            content: 'pong',
+            //ephemeral: true
+        })
+    }
+    else if(commandName ==='add'){
+        const num1 = options.getNumber('num1')!
+        const num2 = options.getNumber('num2')!
+
+        await interaction.deferReply({
+
+        })
+
+        await new Promise(resolve => setTimeout(resolve, 5000)) 
+
+        interaction.editReply({
+            content: `the sum is ${num1 + num2}`
         })
     }
 })
+
 client.login(process.env.TOKEN)
 
 
